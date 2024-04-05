4e323503728221a6b4be52ee681e5f7989b73e92
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2024-04-05 12:26:00 -0300

    cambios en clase 4

---
+++ Clase-4/ejemplo/ejemplo.js
@@ -0,0 +1,7 @@
+const girar=()=>{
+    document.querySelector(".hijo").style.animation="girar 5s infinite"
+}
+
+const pausar=()=>{
+    document.querySelector(".hijo").style.animation="none"
+};
---
+++ Clase-4/script.js
@@ -0,0 +1,9 @@
+const girar=()=>{
+  let contenedorGirar=document.querySelector(".rotate");
+    contenedorGirar.classList.toggle("girar")
+};
+
+const rotarBis=()=>{
+
+  document.querySelector(".combo").classList.add("rotar3D")
+};
